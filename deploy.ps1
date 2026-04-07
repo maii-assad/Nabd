bed8e16a23036d9a3495d4b7154c5c5f9ce6ddbd
Amna Zaher <amnazaher24@gmail.com>
2026-04-07 17:46:55 +0200

    Inject strict default passwords for identity system

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,6 @@
-Remove-Item -Path ".git\.MERGE_MSG.swp" -Force -ErrorAction SilentlyContinue 
-git merge --abort
-git checkout new-doctor
 git add .
-git commit -m "Deploy build fix"
+git commit -m "Inject strict default passwords for identity system"
 git checkout main
-git merge new-doctor -m "Auto Merge branch" --no-edit
+git merge new-doctor -m "Auto merge" --no-edit
 git push origin main
 git checkout new-doctor
